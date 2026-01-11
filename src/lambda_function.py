24d34843996c35d987ccc71c4e329de126555482
thorsanchez <ths281@hi.is>
2026-01-11 15:54:40 +0000

    bætti við s3 upload fyrir csv og png

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,6 +2,9 @@ import matplotlib
 matplotlib.use('Agg')
 import requests
 import matplotlib.pyplot as plt
+import boto3
+from io import BytesIO
+from datetime import datetime
 
 from clean import parse_data, labels
 
@@ -18,6 +21,7 @@ QUERY = {
 }
 
 def lambda_handler(event, context):
+    BUCKET_NAME = "islenska-etl-bucket"
     #extract
     try:
         response = requests.post(API_URL, json=QUERY, timeout=15)
@@ -33,6 +37,36 @@ def lambda_handler(event, context):
     df['Breyta_text'] = df['Breyta'].map(labels)
     df_pivot = df.pivot(index='Ár', columns='Breyta_text', values='value')
 
+    # Hlaða upp csv
+    #búa til s3 client
+    s3 = boto3.client('s3')
+
+    #sækja dags í dag fyrir sjal nafn
+    today = datetime.now().strftime("%Y-%m")
+
+    #búa til csv í memory
+    #tómur buffer
+    csv_buffer = BytesIO()
+    #skrifa DF í buffer (með index fyrir ár)
+    df_pivot.to_csv(csv_buffer, index=True)
+    #endursetja á byrjun
+    csv_buffer.seek(0)
+
+    #hlaða upp á s3
+    #folder/file nafn í s3
+    csv_key = f'data/upplysingataekni_{today}.csv' 
+    try:
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=csv_key,
+            Body=csv_buffer.getvalue(),
+            ContentType='text/csv'
+        )
+        print(f"csv skjal hlaðið upp i s3: {csv_key}")
+    except Exception as e:
+        print(f"error csv ekki hlaðið upp: {str(e)}")
+
+
     print(df_pivot.tail())
 
     #test
@@ -40,6 +74,29 @@ def lambda_handler(event, context):
         df_pivot['Rekstrartekjur (mkr)'].plot(title='Rekstrartekjur í upplýsingageiranum')
         plt.tight_layout()
         plt.savefig("test_plot.png")
+
+        #búa til png í memory
+        img_buffer = BytesIO()
+        #vista plot í buffer
+        plt.savefig(img_buffer, format='png')
+        img_buffer.seek(0)
+        #hreinsa upp plot
+        plt.close()
+
+        #hlaða upp í s3
+        plot_key = f'plots/rekstrartekjur_trend_{today}.png'
+        try:
+            s3.put_object(
+                Bucket=BUCKET_NAME,
+                Key=plot_key,
+                Body=img_buffer.getvalue(),
+                ContentType='image/png'
+            )
+            print(f"png skjal hlaðið upp i s3: {plot_key}")
+        except Exception as e:
+            print(f"error png ekki hlaðið upp: {str(e)}")
+
+
         print("Vistað")
         plt.close()
     except Exception as e:
